5abf93ce884988e654831fac4d2d6455b7b07735
Nestor Alonzo <nalonsoam@gmail.com>
2020-04-27 21:17:54 -0500

    working transcribe function

---
+++ backend/src/lambda/http/transcribeNote.ts
@@ -0,0 +1,44 @@
+import { S3Event, S3Handler } from 'aws-lambda'
+import 'source-map-support/register'
+import { createLogger } from '../../utils/logger'
+import * as AWSXRay from 'aws-xray-sdk'
+import * as AWS from 'aws-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+const logger = createLogger('transcribeNote')
+const transcribeService = new XAWS.TranscribeService()
+const VALID_EXT_REGEXP = /(mp3|mp4|flac|wav)$/
+const transcriptionBucket = process.env.TRANSCRIPTION_S3_BUCKET
+const audioBucket = process.env.ATTACHMENTS_S3_BUCKET
+export const handler: S3Handler = async (event: S3Event): Promise<void> => {
+  const records = event.Records
+
+  for (const rec of records) {
+    const [noteId, fName] = rec.s3.object.key.split('/')
+    const recordUrl = `https://s3.amazonaws.com/${
+      audioBucket}/${
+      rec.s3.object.key
+      }`
+
+    if (!VALID_EXT_REGEXP.exec(fName)) {
+      logger.error('bad-ext', { rec, recordUrl })
+      continue;
+    }
+
+    const [mediaFormat] = VALID_EXT_REGEXP.exec(fName)
+    logger.info({ rec, recordUrl })
+
+    const transcribeParams = {
+      LanguageCode: process.env.LANGUAGE_CODE,
+      Media: { MediaFileUri: recordUrl },
+      MediaFormat: mediaFormat,
+      TranscriptionJobName: noteId,
+      OutputBucketName: transcriptionBucket,
+    }
+
+    logger.info({transcribeParams})
+    const result = await transcribeService.startTranscriptionJob(transcribeParams).promise();
+
+    logger.info({ result })
+  }
+}
