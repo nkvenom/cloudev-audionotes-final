456010c8bf365fe5100bb271ce2bd4d2f81e47d7
Néstor Arias <nesarias@publicisgroupe.net>
2020-06-20 18:47:58 -0500

    fix callback URL for authentication

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,10 @@
 const apiId = 'ytyb4rs16i'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
+const protocol = window.location.protocol;
+
 export const authConfig = {
   domain: 'cd-c4-final.auth0.com',              // Auth0 domain
   clientId: 't33Rngt19Vpsmj4BKyFJ22RauaGz4Jb6', // Auth0 client id
-  callbackUrl: `http://${window.location.host}/callback`
+  callbackUrl: `${protocol}//${window.location.host}/callback`
 }
